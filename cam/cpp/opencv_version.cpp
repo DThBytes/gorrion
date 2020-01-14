04ac67436b7d1654e8198839ba289ac9b7089d69
dt <dt@dt.debian>
2020-01-14 17:22:22 -0300

    opencv base

---
+++ cam/cpp/opencv_version.cpp
@@ -0,0 +1,14 @@
+#include "opencv2/highgui.hpp"
+#include <stdio.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main( int argc, const char** argv )
+{
+	std::cout << "Opencv version: ";
+	std::cout << CV_VERSION << std::endl;
+	return 0;
+}
